12cd077903d3688de204277ea273a2bf5e657a5e
Leonardo Holguin Arias <leonardo9097@hotmail.com>
2024-10-10 17:38:31 -0500

    fix: favicon and main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Leonardo Holguin Arias | Developer',
@@ -25,8 +13,7 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         {children}
       </body>
     </html>
