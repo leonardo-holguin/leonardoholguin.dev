5a4137d38084754d8a7322357e9cb2e6fddfc6e7
Leonardo Holguin Arias <leonardo9097@hotmail.com>
2024-12-23 23:57:18 -0500

    fix: eslint-disable-next-line max-len for reCAPTCHA script line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang='en'>
       <link rel='canonical' href='https://www.leonardoholguin.dev' />
       <script
+        // eslint-disable-next-line max-len
         src='https://www.google.com/recaptcha/api.js?render=6LdGg6QqAAAAANqZ499XllUpeymCqesPrW9nX4RW'
         async></script>
       <body>{children}</body>
