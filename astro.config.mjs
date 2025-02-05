4ae322d62cffb411f06e7f20670bdbb194c4a618
Leonardo Holguin Arias <leonardo9097@hotmail.com>
2025-02-05 01:42:28 -0500

    feat: Background video

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -7,4 +7,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  env: {
+    schema: {
+      PERSONAL_EMAIL: envField.string({ context: "server", access: "public" }),
+    }
+  }
 });
